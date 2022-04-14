c29d786e9ef1a8bac765cff4ba4e49c942d9bb60
Claes Engelin <claes.engelin@zocom.se>
2022-04-14 12:04:48 +0200

    Add project files.

---
+++ Client/Features/Cart/Index.razor.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http.Json;
+
+using Microsoft.AspNetCore.Components;
+
+using ShoppingCartStarter.Shared.Cart;
+
+namespace ShoppingCartStarter.Client.Features.Cart;
+
+public partial class Index
+{
+    [Inject]
+    private HttpClient Http { get; set; }
+    protected Details.Model Model { get; set; }
+    protected override async Task OnInitializedAsync()
+    {
+        await ReloadCart();
+    }
+
+    protected void RemoveItem(Details.Model.LineItem item)
+    {
+        Model.Items.Remove(item);
+    }
+
+    protected async Task ReloadCart()
+    {
+        Model = await Http.GetFromJsonAsync<Details.Model>("api/cart");
+    }
+}
